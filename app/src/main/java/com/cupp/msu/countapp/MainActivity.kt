589ee5b43204e13753b7cb97a67a9f238176c96d
Justin Cupp <jcupp35@gmail.com>
2024-02-03 19:59:40 -0600

    Commit 3

--- app/src/main/java/com/cupp/msu/countapp/MainActivity.kt
+++ app/src/main/java/com/cupp/msu/countapp/MainActivity.kt
@@ -2,10 +2,48 @@ package com.cupp.msu.countapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.cupp.msu.countapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var count: Counter
+    private lateinit var binding: ActivityMainBinding
+
+    //Model
+    private lateinit var btn: Button
+
+    //View
+    private var display: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        class Counter {
+            private var count: Int = 0
+            fun addCount() {
+                count++
+            }
+
+            fun getCount(): Int {
+                return count
+            }
+        }
+
+        count = Counter()
+        display = binding.tv1
+
+        btn = binding.btn1
+
+        btn.setOnClickListener {
+            count.addCount()
+
+            display?.text = count.getCount().toString()
+
+        }
+
     }
 }
